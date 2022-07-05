5a80f0f88864ef415bff69e4ce59d3ce3c6a6fc0
Clara CIOCAN <cc3801875@gitlab.lip6.fr>
2022-07-05 17:13:32 +0200

    added validation

--- validation.sh
+++ validation.sh
@@ -2,7 +2,28 @@
 # ./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/1 -start_frame 1 -end_frame 56 -light_min 60 -light_max 75
 # ./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/2 -start_frame 1 -end_frame 41 -light_min 60 -light_max 75
 # ./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/3 -start_frame 1 -end_frame 51 -light_min 60 -light_max 75
-./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/1 -start_frame 1 -end_frame 56 -light_min 60 -light_max 85
-./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/2 -start_frame 1 -end_frame 41 -light_min 60 -light_max 85
-./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/3 -start_frame 1 -end_frame 51 -light_min 60 -light_max 85
-./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/4 -start_frame 1 -end_frame 41 -light_min 75 -light_max 85 
+# ./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/1 -start_frame 1 -end_frame 56 -light_min 60 -light_max 85
+# ./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/2 -start_frame 1 -end_frame 41 -light_min 60 -light_max 85
+# ./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/3 -start_frame 1 -end_frame 51 -light_min 60 -light_max 85
+# ./ballon -input /dsk/l1/misc/cc3801875/Meteors_in_mkv/4 -start_frame 1 -end_frame 41 -light_min 75 -light_max 85 
+
+# for d in /users/rodata/meteorix/video/2022-TAH/2022-TAH/Videos/mkv/*/ ; do
+    # echo "$d"
+    # for f in /users/rodata/meteorix/video/2022-TAH/2022-TAH/Videos/mkv/Basler_1920/* ; do
+    #     echo "$f"
+    #     ./ballon -input "$f" -start_frame 1 -end_frame 100000
+    # done
+# done
+
+./ballon -input /users/rodata/meteorix/video/2022-TAH/2022-TAH/Videos/mkv/Basler_1920/Basler_acA1920-155um__23278758__20220531_035916822.mkv  -start_frame 1 -end_frame 100000
+./ballon -input /users/rodata/meteorix/video/2022-TAH/2022-TAH/Videos/mkv/Basler_1920/Basler_acA1920-155um__23278758__20220531_042741291.mkv  -start_frame 1 -end_frame 100000
+./ballon -input /users/rodata/meteorix/video/2022-TAH/2022-TAH/Videos/mkv/Basler_1920/Basler_acA1920-155um__23278758__20220531_042259204.mkv  -start_frame 1 -end_frame 100000
+./ballon -input /users/rodata/meteorix/video/2022-TAH/2022-TAH/Videos/mkv/Basler_1920/Basler_acA1920-155um__23278758__20220531_043036585.mkv  -start_frame 1 -end_frame 100000
+
+
+# ./ballon -input /users/rodata/meteorix/video/Videos\ mkv\ TAH2022/Basler1920/Basler_acA1920-155um__23278758__20220531_043414241.mkv  -start_frame 1 -end_frame 100000
+# ./ballon -input /users/rodata/meteorix/video/Videos\ mkv\ TAH2022/Basler1920/Basler_acA1920-155um__23278758__20220531_044700189.mkv  -start_frame 1 -end_frame 100000
+# ./ballon -input /users/rodata/meteorix/video/Videos\ mkv\ TAH2022/Basler1920/Basler_acA1920-155um__23278758__20220531_045802638.mkv  -start_frame 1 -end_frame 100000
+# ./ballon -input /users/rodata/meteorix/video/Videos\ mkv\ TAH2022/Basler1920/Basler_acA1920-155um__23278758__20220531_062411027.mkv  -start_frame 1 -end_frame 100000
+# ./ballon -input /users/rodata/meteorix/video/Videos\ mkv\ TAH2022/Basler_1920/Basler_acA1920-155um__23278758__20220531_034611281.mkv  -start_frame 1 -end_frame 100000
+# ./ballon -input /users/rodata/meteorix/video/Videos\ mkv\ TAH2022/Basler_1920/Basler_acA1920-155um__23278758__20220531_040424097.mkv  -start_frame 1 -end_frame 100000
