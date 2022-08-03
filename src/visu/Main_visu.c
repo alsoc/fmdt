380b6e8567dcffd0774be5751221068c9b6ec890
Adrien Cassagne <adrien.cassagne@lip6.fr>
2022-08-03 11:15:56 +0200

    Improve stdout for 'meteor-visu'.

--- src/visu/Main_visu.c
+++ src/visu/Main_visu.c
@@ -308,7 +308,16 @@ void main_visu(int argc, char** argv)
     
     // recupere les tracks
     parseTracks(src_path, tracks, &nb_tracks);
-    printTracks(tracks, nb_tracks);
+    //printTracks(tracks, nb_tracks);
+
+    unsigned n_tracks = 0, n_stars = 0, n_meteors = 0, n_noise = 0;
+    n_tracks = count_objects(tracks, nb_tracks, &n_stars, &n_meteors, &n_noise);
+
+    printf("# Read from tracks file:\n");
+    printf("# -> Number of meteor objects: %5d\n", n_meteors);
+    printf("# -> Number of star objects:   %5d\n", n_stars);
+    printf("# -> Number of noise objects:  %5d\n", n_noise);
+    printf("# -> Total number of objects:  %5d\n", n_tracks);
     
     // init 
     Video* video = Video_init_from_file(src_path_video, start, end, 0, &i0, &i1, &j0, &j1);
