190d3caf33f557607db8233cc3534f6e93f0b7e0
Adrien Cassagne <adrien.cassagne@lip6.fr>
2022-12-12 14:46:39 +0100

    CI: fix to work with the new regression script.

--- ci/test-coverage-regression.sh
+++ ci/test-coverage-regression.sh
@@ -4,7 +4,7 @@ set -x
 WD=$(pwd)
 build_root=build
 
-cd scripts
+cd scripts/regression
 curl https://lip6.fr/adrien.cassagne/data/tauh/in/2022_05_31_tauh_34_meteors.mp4 --output 2022_05_31_tauh_34_meteors.mp4
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 curl https://lip6.fr/adrien.cassagne/data/fmdt/refs_2022_05_31_tauh_34_meteors.zip --output refs.zip
@@ -12,13 +12,13 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 unzip refs.zip
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
-python3 compare.py --in-video ${WD}/scripts/2022_05_31_tauh_34_meteors.mp4 --refs-path ${WD}/scripts/refs
+./compare.py --exe-args "--in-video ${WD}/scripts/regression/2022_05_31_tauh_34_meteors.mp4 --track-all" --list-exe "fmdt-detect, fmdt-detect-rt-pip, fmdt-detect-rt-seq" --refs-path ${WD}/scripts/regression/refs
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 cd ${WD}
 
 mkdir code_coverage_files || true
-for Exe in 'fmdt-common-obj' 'fmdt-detect-common-obj' 'fmdt-detect-rt-common-obj' 'fmdt-detect-exe' 'fmdt-detect-rt-pip-exe' 'fmdt-detect-rt-seq-exe' 'fmdt-detect-rt2-exe'; do
+for Exe in 'fmdt-common-obj' 'fmdt-detect-common-obj' 'fmdt-detect-rt-common-obj' 'fmdt-detect-exe' 'fmdt-detect-rt-pip-exe' 'fmdt-detect-rt-seq-exe'; do
     lcov --capture --directory $build_root/CMakeFiles/${Exe}.dir/src --output-file code_coverage_files/${Exe}.info
     rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
     cp code_coverage_files/${Exe}.info code_coverage_files/${Exe}2.info
