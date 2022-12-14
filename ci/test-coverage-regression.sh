d00ec4e7f43c8104564bbaee3810572ea63869b6
Adrien Cassagne <adrien.cassagne@lip6.fr>
2022-12-14 20:28:38 +0100

    CI: update the refs for regression tests.

--- ci/test-coverage-regression.sh
+++ ci/test-coverage-regression.sh
@@ -7,10 +7,11 @@ build_root=build
 cd scripts/regression
 curl https://lip6.fr/adrien.cassagne/data/tauh/in/2022_05_31_tauh_34_meteors.mp4 --output 2022_05_31_tauh_34_meteors.mp4
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-curl https://lip6.fr/adrien.cassagne/data/fmdt/refs_2022_05_31_tauh_34_meteors.zip --output refs.zip
+curl https://lip6.fr/adrien.cassagne/data/fmdt/refs_2022_05_31_tauh_34_meteors_296a5d56.zip --output refs.zip
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 unzip refs.zip
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+rm refs/tracks.txt
 
 ./compare.py --exe-args "--in-video ${WD}/scripts/regression/2022_05_31_tauh_34_meteors.mp4 --track-all" --list-exe "fmdt-detect, fmdt-detect-rt-pip, fmdt-detect-rt-seq" --refs-path ${WD}/scripts/regression/refs
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
