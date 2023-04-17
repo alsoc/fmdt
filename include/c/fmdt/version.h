04c971c8ad9064054c2341075231e7fea17d56b8
Adrien Cassagne <adrien.cassagne@lip6.fr>
2023-04-17 14:59:36 +0200

    Doc: version + use integers for major, minor, patch and build.

--- include/c/fmdt/version.h
+++ include/c/fmdt/version.h
@@ -5,10 +5,26 @@
 
 #pragma once
 
+/** Git full SHA1 hash */
 extern char g_fmdt_sha1[256];
-extern char g_fmdt_version[256];
-extern char g_fmdt_version_major[256];
-extern char g_fmdt_version_minor[256];
-extern char g_fmdt_version_patch[256];
 
+/** Git full version, in the following form: vM.m.p-b-g[hash7] (example : v1.0.0-2-g1dd9353) */
+extern char g_fmdt_version[256];
+
+/** Git major version M */
+extern unsigned g_fmdt_version_major;
+
+/** Git minor version m */
+extern unsigned g_fmdt_version_minor;
+
+/** Git patch p */
+extern unsigned g_fmdt_version_patch;
+
+/** Git build b */
+extern unsigned g_fmdt_build;
+
+/**
+ * Print the FMDT version in the standard output.
+ * @param bin_name Name of the current executable.
+ */
 void print_version(const char* bin_name);
