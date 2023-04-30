0c81124fb262aa3848265aecc9229d087f601301
Adrien Cassagne <adrien.cassagne@lip6.fr>
2023-04-30 11:02:36 +0200

    CI: update 'fmdt-detect' regression tests.

--- ci/test-regression.sh
+++ ci/test-regression.sh
@@ -28,23 +28,23 @@ else
                      fmdt-detect-opt"
 fi
 
-curl https://lip6.fr/adrien.cassagne/data/fmdt/refs_detect_2022_05_31_tauh_34_meteors_b34a8a1f.zip --output refs.zip
+curl https://lip6.fr/adrien.cassagne/data/fmdt/refs_detect_2022_05_31_tauh_34_meteors_977a7ea9.zip --output refs_detect.zip
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
-unzip refs.zip
+unzip refs_detect.zip
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 ./compare.py --build-path ${WD}/${build_root}/bin/ \
              --list-exe "${list_exe_detect}" \
              --exe-args "--vid-in-path ${WD}/scripts/regression/2022_05_31_tauh_34_meteors.mp4 --ccl-impl \"LSLH\" --trk-all --cca-mag --cca-ell --ccl-fra-path ${out_root_dir}/%05d.pgm --log-path ${out_root_dir}" \
              --out-path "${out_root_dir}" \
-             --refs-path ${WD}/scripts/regression/refs
+             --refs-path ${WD}/scripts/regression/refs_detect
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 ./compare.py --build-path ${WD}/${build_root}/bin/ \
              --list-exe "${list_exe_detect}" \
              --exe-args "--vid-in-path ${WD}/scripts/regression/2022_05_31_tauh_34_meteors.mp4 --ccl-impl \"LSLM\" --trk-all --cca-mag --cca-ell --ccl-fra-path ${out_root_dir}/%05d.pgm --log-path ${out_root_dir}" \
              --out-path "${out_root_dir}" \
-             --refs-path ${WD}/scripts/regression/refs
+             --refs-path ${WD}/scripts/regression/refs_detect
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 # -----------------------------------------------------------------------------
