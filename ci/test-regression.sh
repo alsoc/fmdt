7522f1c45995d502118c20bc95b9425eaa669bb7
Adrien Cassagne <adrien.cassagne@lip6.fr>
2023-05-12 10:45:40 +0200

    CI: add 'fmdt-detect-no-fail' in regression tests.

--- ci/test-regression.sh
+++ ci/test-regression.sh
@@ -17,6 +17,7 @@ rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 if test -f "${WD}/${build_root}/bin/fmdt-detect-rt-seq"; then
     list_exe_detect="fmdt-detect, \
                      fmdt-detect-opt, \
+                     fmdt-detect-no-fail, \
                      fmdt-detect-rt-pip, \
                      fmdt-detect-rt-seq, \
                      fmdt-detect-rt2-pip, \
@@ -25,7 +26,8 @@ if test -f "${WD}/${build_root}/bin/fmdt-detect-rt-seq"; then
                      fmdt-detect-rt-opt-pip"
 else
     list_exe_detect="fmdt-detect, \
-                     fmdt-detect-opt"
+                     fmdt-detect-opt, \
+                     fmdt-detect-no-fail"
 fi
 
 curl https://lip6.fr/adrien.cassagne/data/fmdt/refs_detect_2022_05_31_tauh_34_meteors_62d79311.zip --output refs_detect.zip
