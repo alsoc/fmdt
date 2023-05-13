9f729ff9f14d7ae8053a9e41c6a8837af65ccbe0
Adrien Cassagne <adrien.cassagne@lip6.fr>
2023-05-13 13:35:53 +0200

    CI: add missing exe in memory tests.

--- ci/test-memory.sh
+++ ci/test-memory.sh
@@ -6,12 +6,17 @@ vid_path="../2022_05_31_tauh_34_meteors.mp4"
 log_path="out_logs"
 
 list_of_cmds=(
-    "fmdt-ellipse        --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm'"
-    "fmdt-detect         --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
-    "fmdt-detect-rt-seq  --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
-    "fmdt-detect-rt-pip  --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
-    "fmdt-detect-rt2-seq --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
-    "fmdt-detect-rt2-pip --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
+    "fmdt-ellipse            --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm'"
+    "fmdt-detect             --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
+    "fmdt-detect-no-fail     --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
+    "fmdt-detect-opt         --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
+    "fmdt-detect-opt-no-fail --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
+    "fmdt-detect-rt-seq      --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
+    "fmdt-detect-rt-pip      --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
+    "fmdt-detect-rt2-seq     --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
+    "fmdt-detect-rt2-pip     --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
+    "fmdt-detect-rt-opt-seq  --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
+    "fmdt-detect-rt-opt-pip  --vid-in-start 0 --vid-in-stop 20 --vid-in-path $vid_path --log-path $log_path --ccl-fra-path '$frame_dir/%05d.pgm' --trk-all --trk-star-min 15 --cca-mag"
 )
 
 wget https://lip6.fr/adrien.cassagne/data/tauh/in/2022_05_31_tauh_34_meteors.mp4 .
