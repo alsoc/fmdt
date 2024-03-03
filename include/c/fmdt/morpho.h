7ce500248286d31199b26413483248b55e8e16c1
Adrien Cassagne <adrien.cassagne@lip6.fr>
2024-03-03 09:21:25 +0100

    Remove 'fmdt-detect-motion'.

--- include/c/fmdt/morpho.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "fmdt/morpho/morpho_struct.h"
-#include "fmdt/morpho/morpho_compute.h"
--- include/c/fmdt/sigma_delta.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include "fmdt/sigma_delta/sigma_delta_struct.h"
-#include "fmdt/sigma_delta/sigma_delta_compute.h"
